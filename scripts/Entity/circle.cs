fb298ea92dcc1db0eae2600548469f95b93e30d8
손성현 <thstjdgus0813@gmail.com>
2025-08-18 00:10:00 +0900

    공 렌덤 생성 기능 값은 나중에 세부 조정

--- scripts/Entity/circle.cs
+++ scripts/Entity/circle.cs
@@ -4,21 +4,34 @@ public class circle : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] private float speedX = 10; // 속도 설정
-
     [SerializeField] private float speedY = 10; // 속도 설정
-
+    //공이 생성되는 최댓값과 최솟값 지정 나중에 Inspector에서 조정 가능
+    [SerializeField] private float minX = -10;
+    [SerializeField] private float maxX = 10;
+    [SerializeField] private float minY = -10;
+    [SerializeField] private float maxY = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        rb.linearVelocity = new Vector2(speedX, speedY); // Rigidbody2D의 속도를 설정
+        rb.linearVelocity = new Vector2(speedX, speedY);
+        CircleSpawn(); // Rigidbody2D의 속도를 설정
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public float CircleSpawn()
+    {
+        
+        float RandomX = Random.Range(minX, maxX);
+        float RandomY = Random.Range(minY, maxY);
+        transform.position = new Vector2(RandomX, RandomY);
+        return RandomX;
     }
 }
     
