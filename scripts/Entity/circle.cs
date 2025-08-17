7c7859677a1ddf4383406f0e1bec94840d73d994
손성현 <thstjdgus0813@gmail.com>
2025-08-17 23:36:30 +0900

    공 포물선으로 바꿈

--- scripts/Entity/circle.cs
+++ scripts/Entity/circle.cs
@@ -3,25 +3,22 @@ using UnityEngine;
 public class circle : MonoBehaviour
 {
     private Rigidbody2D rb;
-    [SerializeField] private float speed = 10; // 속도 설정
+    [SerializeField] private float speedX = 10; // 속도 설정
+
+    [SerializeField] private float speedY = 10; // 속도 설정
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        rb.linearVelocity = new Vector2(speedX, speedY); // Rigidbody2D의 속도를 설정
     }
 
     // Update is called once per frame
     void Update()
     {
-        CircleMove(Vector2.left);
-    }
-    /* I는 이동 방향을 나타내는 벡터
-             speed는 이동 속도
-             rb는 Rigidbody2D 컴포넌트로, 물리 기반의 이동을 처리
-             linearVelocity를 사용하여 Rigidbody2D의 속도를 설정.*/ 
-    private void CircleMove(Vector2 I)
-        
-    {
-        rb.linearVelocity = speed * I; // Rigidbody2D를 사용하여 위치 이동
+
     }
 }
+    
